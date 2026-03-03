29b4cb7e905aa28edb952a6b063de62318c8e66a
Nitesh Kumar <niteshkumar@MacBook-Pro.local>
2026-03-03 20:24:00 +0000

    feat: wire MaterialApp and HomeScreen in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const RecipeBookApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class RecipeBookApp extends StatelessWidget {
+  const RecipeBookApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Quick Weeknight Recipe Studio',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        scaffoldBackgroundColor: Colors.grey[100],
       ),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
